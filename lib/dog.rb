6663c7bf6ad27519f97e8d48f9f2c25a288caa57
Unsar Malik <unsar.malik24@gmail.com>
2019-05-05 02:01:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,12 +10,9 @@ class Dog
   end
   
   def self.all
-    @@all.
+    @@all.each do |dog|
+      puts
   end
-  
-pluto = Dog.new("Pluto")
-fido = Dog.new("Fido")
-maddy = Dog.new("Maddy")
 
   Dog.all
 end
