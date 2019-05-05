259bc4a8a91fe7bc1fa5abffe1ace03e059eed6e
Unsar Malik <unsar.malik24@gmail.com>
2019-05-05 01:59:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,12 +9,13 @@ class Dog
     @@all << self
   end
   
+  def self.all
+    @@all
+  end
+  
   pluto=Dog.new("Pluto")
   fido=Dog.new("Fido")
   maddy=Dog.new("Maddy")
 
-  def self.all
-    @@all
-  end
   Dog.all
 end
